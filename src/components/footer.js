500d24293a7fcbcdff288714492c56ce38c23012
unknown <sekiseki85@gmail.com>
2021-07-28 12:22:21 +0200

    restore1

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ import * as React from 'react'
 import {StaticImage} from 'gatsby-plugin-image'
 import LinkeWhite from '../images/linkedin-white.png'
 import '../styles/styles.css'
+//test restore
 
 
 const Footer = ({data}) => {
