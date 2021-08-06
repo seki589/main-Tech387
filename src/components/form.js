53539fc7280333d452fbaa3751c48db21383e792
unknown <sekiseki85@gmail.com>
2021-08-06 11:50:07 +0200

    edit form

--- src/components/form.js
+++ src/components/form.js
@@ -1,32 +1,52 @@
+
 import React from 'react'
+import {useState} from 'react'
 
 
 const Form = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [project, setProject] = useState('');
 
-   return(
-       <div className="hireUsForm">
-           <form>
-               <input
-               type="text"
-               name="name"
-               required
-               placeholder="Name"
-               ></input>
-               <input
-               type="text"
-               name="email"
-               required
-               placeholder="Email"
-               ></input>
-               <input
-               type="text"
-               name="details"
-               placeholder="Project Details (optional)"
-               ></input>
-               <button className="send">Send</button>
-           </form>
-       </div>
-   )
+    const handleSent = (e) => {
+        e.preventDefault();
+        const mail = {name, email, project}
+        console.log(mail);
+        setName('');
+        setEmail('');
+        setProject('');
+    }
+
+        return (
+            <div className="hireUsForm">
+                <form onSubmit={handleSent}>
+                    <input
+                            type="text"
+                            name="name"
+                            required
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Name"
+                    ></input>
+                    <input
+                            type="text"
+                            name="email"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email"
+                    ></input>
+                    <input
+                            type="text"
+                            name="project"
+                            value={project}
+                            onChange={(e) => setProject(e.target.value)}
+                            placeholder="Project Details (optional)"
+                    ></input>
+                    <button className="send">Send</button>
+                </form>
+            </div>
+        )
 
 
 }
