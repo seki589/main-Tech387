618f76f7d63637eec85e927df5896fd92179a24a
unknown <sekiseki85@gmail.com>
2021-08-06 09:43:32 +0200

    some styling

--- src/components/form.js
+++ src/components/form.js
@@ -8,16 +8,19 @@ const Form = () => {
            <form>
                <input
                type="text"
+               name="name"
                required
                placeholder="Name"
                ></input>
                <input
                type="text"
+               name="email"
                required
                placeholder="Email"
                ></input>
                <input
                type="text"
+               name="details"
                placeholder="Project Details (optional)"
                ></input>
                <button className="send">Send</button>
