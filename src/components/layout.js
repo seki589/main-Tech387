87d4b84d9ea87bad80d93196672e85911cd92d47
unknown <sekiseki85@gmail.com>
2021-09-16 01:52:51 +0200

    fix mobile height

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,18 +41,10 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          // maxWidth: 960,
-          // padding: `0 1.0875rem 1.45rem`,
+
         }}
       >
         <main >{children}</main>
-        <footer
-          // style={{
-          //   marginTop: `2rem`,
-          // }}
-        >
-
-        </footer>
       </div>
     </>
   )
