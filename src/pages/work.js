5c4d352a1c3b85416dadb3779615fed4f8a5ddd1
unknown <sekiseki85@gmail.com>
2021-09-27 12:23:27 +0200

    edit pdf path

--- src/pages/work.js
+++ src/pages/work.js
@@ -43,7 +43,7 @@ const WorkPage = () => (
             <section className={`work-container`}>
                 <div className="work-midle">
                     <div className="midle-top">
-                        <a className="card" href="./pdf/Spartan.pdf" target="_blank" rel="noreferrer">
+                        <a className="card" href={`/pdf/Spartan.pdf`}  target="_blank" rel="noreferrer">
                         <StaticImage src="../images/spartanApp.png" className="plant-2" alt="Spartan" placeholder="blurred"/>
                         <h6>Spartan</h6>
                         <p> Personalised training and nutrition app designed to help you reach your goals </p>  
