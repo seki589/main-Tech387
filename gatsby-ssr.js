e18adaf7538e43259d206b33cb32d7627db5a3ee
unknown <sekiseki85@gmail.com>
2021-09-28 16:02:15 +0200

    edit meta description

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,21 +1,41 @@
-exports.onPreRenderHTML = function onPreRenderHTML({
-    getHeadComponents,
-    replaceHeadComponents,
-  }) {
-    const headComponents = getHeadComponents();
-    headComponents.sort((a, b) => {
-      if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
-        return 0;
-      }
+import React from "react"
+import { Helmet } from "react-helmet"
+
+export const onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+  const helmet = Helmet.renderStatic()
+  setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent()
+  ])
+}
+
+// exports.onPreRenderHTML = function onPreRenderHTML({
+//     getHeadComponents,
+//     replaceHeadComponents,
+//   }) {
+//     const headComponents = getHeadComponents();
+//     headComponents.sort((a, b) => {
+//       if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+//         return 0;
+//       }
   
-      if (a.type === 'style') {
-        return 1;
-      } else if (b.type === 'style') {
-        return -1;
-      }
+//       if (a.type === 'style') {
+//         return 1;
+//       } else if (b.type === 'style') {
+//         return -1;
+//       }
   
-      return 0;
-    });
+//       return 0;
+//     });
   
-    replaceHeadComponents(headComponents);
-  };
+//     replaceHeadComponents(headComponents);
+//   };
